3d1d6a7b512694d196b613fea720b196a4791634
yvanesc <yvan.esc@gmail.com>
2018-11-28 00:16:46 +0100

    up

--- dev01.py
+++ dev01.py
@@ -53,7 +53,7 @@ time.sleep(3)
 clock = pygame.time.Clock()
 while True:
         os.system('clear')
-        DISPLAYSURF.fill(iniPi.GREYDARK)
+        DISPLAYSURF.fill(iniPi.WHITE)
         time2Display = datetime.datetime.now().strftime("%H:%M")
         hour2Display = int(datetime.datetime.now().strftime("%H"))        
         date2Display = datetime.datetime.now().strftime("%d")
