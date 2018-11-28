461aeea4a3048e0f61579cba39f658b8a9f0f439
yvanesc <yvan.esc@gmail.com>
2018-11-28 01:48:35 +0100

    up

--- devMenu01.py
+++ devMenu01.py
@@ -60,7 +60,7 @@ while True:
                  
         
         #screen
-        pygame.draw.rect(DISPLAYSURF, iniPi.RED, (32,50,256,30), 5)
+        DISPLAYSURF.draw.rect(DISPLAYSURF, iniPi.RED, (32,50,256,30), 5)
         DISPLAYSURF.blit(icO, (icOPosX, icOPosY))
         DISPLAYSURF.blit(icX, (icXPosX, icXPosY))
         DISPLAYSURF.blit(icDown, (icDownPosX, icDownPosY))
