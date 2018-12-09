e4c4d0cfffe6f57e29dc8456f2c66efce268ca5c
yvanesc <yvan.esc@gmail.com>
2018-12-09 23:21:11 +0100

    up

--- guiMenu01.py
+++ guiMenu01.py
@@ -114,7 +114,8 @@ while True:
                 displayTimed = pygame.transform.rotate(displayTime,angleRot)   
                 displayDated = pygame.transform.rotate(displayDate,angleRot)   
                 DISPLAYSURF.blit(displayTimed, ((scrW/4)+marge, icOPosY*2))
-                DISPLAYSURF.blit(displayDated, ((scrW/2)+marge, icRectPosY*2))        
+                DISPLAYSURF.blit(displayTimed, ((scrW/2)+(marge*6), 120))
+                DISPLAYSURF.blit(displayDated, ((scrW/2)+marge, icTriPosY*2))        
 
         DISPLAYSURF.blit(icOd, (icOPosX*4, icOPosY*2))
         DISPLAYSURF.blit(icXd, (icXPosX*4, icXPosY*2))
