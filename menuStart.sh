785cfdf8c20e8f5ad5a7adfb2dd38ca15a4aa523
yvanesc <yvanesc>
2018-12-23 13:26:03 +0100

    menu start add

---
+++ menuStart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "select the operation ************  1)operation 1 2)operation 2 3)operation 3 4)operation 4 "
+
+read n
+case $n in
+    1) commands for opn 1;;
+    2) commands for opn 2;;
+    3) commands for opn 3;;
+    4) commands for  opn 4;;
+    *) invalid option;;
+esac
