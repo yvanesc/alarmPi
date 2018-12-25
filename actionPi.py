e9e9b7cd15db51091fdd17cd527703033b42474d
yvanesc <yvan.esc@gmail.com>
2018-12-25 21:26:39 +0100

    fx

--- actionPi.py
+++ actionPi.py
@@ -5,8 +5,8 @@ def actFormatTime(hr, min):
 	elif alMin < 10:
 		alarm2Display = str(alHour) + ":" + "0" + str(alMin)
 	elif alHour < 10:
-    	alarm2Display = "0" + str(alHour) + ":" + str(alMin)
-    else:
-    	alarm2Display = str(alHour) + ":" + str(alMin)   
+		alarm2Display = "0" + str(alHour) + ":" + str(alMin)
+	else:
+		alarm2Display = str(alHour) + ":" + str(alMin)
 
 	return(alarm2Display)
