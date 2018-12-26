683bfe5dee827fb60f409abbe0ce3e3d5390150f
yvanesc <yvan.esc@gmail.com>
2018-12-26 22:01:51 +0100

    fx

--- bluezPi.py
+++ bluezPi.py
@@ -9,7 +9,12 @@ def search():
 		devices = bluetooth.discover_devices(duration=10, lookup_names=1, flush_cache=1)
 		for device_address, device_name in devices:
 			#print("Found: {}".format(device_name))
-			print(bluezDeviceFnd.index(device_name))
+			try:
+				bluezDeviceFnd.index(device_name)
+			#print(bluezDeviceFnd.index(device_name))
+			except IndexError:
+				bluezDeviceFnd.append(device_name)
+
 			#if bluezDeviceFnd.index(device_name)
 			#print(device_name)
         #if(device_name.lower() == "chrisg"):
