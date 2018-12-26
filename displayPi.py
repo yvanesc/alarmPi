4868b85aa28d210eef92a6c4321b2c1a55f78c2b
yvanesc <yvan.esc@gmail.com>
2018-12-26 21:01:57 +0100

    fx

--- displayPi.py
+++ displayPi.py
@@ -11,3 +11,5 @@ def dayDis():
 	icTri=pygame.image.load(ic32PathS+ "loop-circular" +ic32PathE)
 	icUp=pygame.image.load(ic32PathS+ "caret-top" +ic32PathE)
 	icDown=pygame.image.load(ic32PathS+ "caret-bottom" +ic32PathE)
+
+	return icO, icX, icRect, icTri, icUp, icDown
