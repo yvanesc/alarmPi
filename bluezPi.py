de7d27eb54c985a6155c80dc85b0ca2244216767
yvanesc <yvan.esc@gmail.com>
2018-12-26 21:38:30 +0100

    fx

--- bluezPi.py
+++ bluezPi.py
@@ -3,14 +3,9 @@ import bluetooth
 
 def search(): 
 	while 1:
- 
-    	print("Searching ...")
- 
-    	devices = bluetooth.discover_devices(duration=10, lookup_names=1, flush_cache=1)
- 
-    	for device_address, device_name in devices:
- 
-        	print("Found: {}".format(device_name))
- 		
+		print("Searching ...")
+		devices = bluetooth.discover_devices(duration=10, lookup_names=1, flush_cache=1)
+		for device_address, device_name in devices:
+			print("Found: {}".format(device_name))
         #if(device_name.lower() == "chrisg"):
             #os.system("mplayer /home/pi/imperial.mp3")
