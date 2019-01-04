23bb526cfe9f4fc528907f969b32b1b3738b2caf
yvanesc <yvan.esc@gmail.com>
2019-01-04 22:04:52 +0100

    up

--- guiMenu02.py
+++ guiMenu02.py
@@ -68,12 +68,21 @@ while True:
         disInfoTxt = fontSel.render("2", True, iniPi.BLACK)
         pygame.draw.rect(DISPLAYSURF, GREEN, (0,(scrH/4),(scrW/8),(scrH/4)), 6)
         disInfoTxt = pygame.transform.rotate(disInfoTxt,90)
+
         DISPLAYSURF.blit(disInfoTxt, ((scrW/8)/2 - disInfoTxt.get_width()/2,  (scrH/4)+ (scrH/4)/2 - disInfoTxt.get_height()/2))
         pygame.draw.rect(DISPLAYSURF, GREY, (0,(scrH/2),(scrW/8),(scrH/4)), 6)
+        disInfoTxt = fontSel.render("3", True, iniPi.BLACK)
+        DISPLAYSURF.blit(disInfoTxt, ((scrW/8)/2 - disInfoTxt.get_width()/2,  (scrH/4)+ (scrH/2) - disInfoTxt.get_height()/2)
         pygame.draw.rect(DISPLAYSURF, RED, (0,(scrH/4)*3,(scrW/8),(scrH/4)), 6)
+        disInfoTxt = fontSel.render("4", True, iniPi.BLACK)
+        DISPLAYSURF.blit(disInfoTxt, ((scrW/8)/2 - disInfoTxt.get_width()/2,  scrH - disInfoTxt.get_height()/2)
         #center
         pygame.draw.rect(DISPLAYSURF, BLACK, ((scrW/8),0,(scrW/2)-(scrW/8),(scrH/2)), 6)
+        disInfoTxt = fontSel.render("5", True, iniPi.BLACK)
+        DISPLAYSURF.blit(disInfoTxt, ((scrW/2 - scrW/8)/2 + scrW/8 - disInfoTxt.get_width()/2,  scrH/4 - disInfoTxt.get_height()/2)
         pygame.draw.rect(DISPLAYSURF, GREYDARK, ((scrW/8),(scrH/2),(scrW/2)-(scrW/8),(scrH/2)), 6)
+        disInfoTxt = fontSel.render("6", True, iniPi.BLACK)
+        DISPLAYSURF.blit(disInfoTxt, ((scrW/2 - scrW/8)/2 + scrW/8 - disInfoTxt.get_width()/2,  scrH/2 + scrH/2 - disInfoTxt.get_height()/2)
         pygame.draw.rect(DISPLAYSURF, RED, ((scrW/2),0,(scrW/2)-(scrW/8),(scrH/2)), 6)
         pygame.draw.rect(DISPLAYSURF, GREEN, ((scrW/2),(scrH/2),(scrW/2)-(scrW/8),(scrH/2)), 6)
         #last col and 3 row
