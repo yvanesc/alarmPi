e0a5f1d418bb88deef8449aa50efc3dcc48cd868
yvanesc <yvan.esc@gmail.com>
2019-01-04 22:34:45 +0100

    fx

--- guiMenu02.py
+++ guiMenu02.py
@@ -82,7 +82,7 @@ while True:
         DISPLAYSURF.blit(disInfoTxt, ((scrW/2 - scrW/8)/2 + scrW/8 - disInfoTxt.get_width()/2,  scrH/4 - disInfoTxt.get_height()/2))
         pygame.draw.rect(DISPLAYSURF, GREYDARK, ((scrW/8),(scrH/2),(scrW/2)-(scrW/8),(scrH/2)), 6)
         disInfoTxt = fontSel.render("6", True, iniPi.BLACK)
-        DISPLAYSURF.blit(disInfoTxt, ((scrW/2 - scrW/8)/2 + scrW/8 - disInfoTxt.get_width()/2,  scrH/2 + scrH/2 - disInfoTxt.get_height()/2)
+        DISPLAYSURF.blit(disInfoTxt, ((scrW/2 - scrW/8)/2 + scrW/8 - disInfoTxt.get_width()/2,  scrH/2 + scrH/2 - disInfoTxt.get_height()/2))
         pygame.draw.rect(DISPLAYSURF, RED, ((scrW/2),0,(scrW/2)-(scrW/8),(scrH/2)), 6)
         pygame.draw.rect(DISPLAYSURF, GREEN, ((scrW/2),(scrH/2),(scrW/2)-(scrW/8),(scrH/2)), 6)
         #last col and 3 row
