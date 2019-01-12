1e9447d3da80d756edf26a31bf388ff44cf21078
yvanesc <yvan.esc@gmail.com>
2019-01-12 14:46:15 +0100

    up

--- displayPi.py
+++ displayPi.py
@@ -395,7 +395,7 @@ def scrSaveT02(DISPLAYSURF,scrW,scrH):
 
 		# Move the snow flake down one pixel
 		#snow_list[i][1] += 4
-		snow_list[i][1] += random.randrange (2, 6)
+		snow_list[i][1] += random.randrange (1, 8)
 
 		# If the snow flake has moved off the bottom of the screen
 		if snow_list[i][1] > scrH :#or (snow_list[i][1] > 51) and snow_list[i][0] > (scrW - lenDisTime - marge)):        
