7d36afdba74a11c8a581f27c6d61d0ce8d1d98bf
yvanesc <yvan.esc@gmail.com>
2019-01-12 14:24:13 +0100

    fx

--- displayPi.py
+++ displayPi.py
@@ -372,7 +372,8 @@ def scrSaveT02(DISPLAYSURF,scrW,scrH):
 		pygame.draw.circle(DISPLAYSURF, WHITE, (posSnX, posSnY), 1)
 
 		# Move the snow flake down one pixel
-		snow_list[i][1] += 4
+		#snow_list[i][1] += 4
+		snow_list[i][1] += random.randrange (4, 8)
 
 		# If the snow flake has moved off the bottom of the screen
 		if snow_list[i][1] > scrH :#or (snow_list[i][1] > 51) and snow_list[i][0] > (scrW - lenDisTime - marge)):        
