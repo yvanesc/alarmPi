bab56f447968c1dbb799deb6f513010d73cf2e86
yvanesc <yvan.esc@gmail.com>
2019-01-13 00:44:56 +0100

    fx

--- displayPi.py
+++ displayPi.py
@@ -609,13 +609,13 @@ def scrSaveRain(DISPLAYSURF,scrW,scrH):
 			# Give it a new x position
 			x = random.randrange(0, scrW)
 			snow_list[i][0] = x
-		if (snow_list[i][1] > 53 and snow_list[i][0]) > (scrW - lenDisTime - marge*2):
+		if (snow_list[i][1] > 53) and ((snow_list[i][0]) > (scrW - lenDisTime - marge*2)):
 			#snow_list[i][1] = snow_list[i][1] + 400
 			y = random.randrange(-50, -10)
 			snow_list[i][1] = y
 			# Give it a new x position
 			x = random.randrange(0, scrW)
-			
+
 	# Go ahead and update the screen with what we've drawn.
 	DISPLAYSURF.blit(displayTime, (scrW - lenDisTime - marge, 52)) 
 	DISPLAYSURF.blit(displayDate, (scrW - lenDisTime - marge, 182)) 
