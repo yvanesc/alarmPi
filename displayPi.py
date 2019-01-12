58e7b240c321f1066213e4c88a2886fa2327fc11
yvanesc <yvan.esc@gmail.com>
2019-01-13 00:47:07 +0100

    up

--- displayPi.py
+++ displayPi.py
@@ -594,8 +594,8 @@ def scrSaveRain(DISPLAYSURF,scrW,scrH):
  
 		# Draw the snow flake
 		pygame.draw.circle(DISPLAYSURF, WHITE, snow_list[i], 2)		
-		for iTrace in range(1,255,1):
-			pygame.draw.circle(DISPLAYSURF, (255-iTrace, 255-iTrace, 255-iTrace), (snow_list[i][0],snow_list[i][1]-iTrace), 1)
+		for iTrace in range(1,255,2):
+			pygame.draw.circle(DISPLAYSURF, (255-iTrace, 255-iTrace, 255-iTrace), (snow_list[i][0],snow_list[i][1]-iTrace), 2)
 		# Move the snow flake down one pixel
 		snow_list[i][1] += 1
 
