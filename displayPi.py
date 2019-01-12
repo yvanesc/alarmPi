ddd9e882c959f993c55868e32cdde7f3930c2cf6
yvanesc <yvan.esc@gmail.com>
2019-01-12 22:12:07 +0100

    up

--- displayPi.py
+++ displayPi.py
@@ -377,11 +377,11 @@ def scrSaveT02(DISPLAYSURF,scrW,scrH):
 		posSnY = snow_list[i][1]
 		posSnY -= random.randrange (10, 35)
 		posSnX = snow_list[i][0]
-		pygame.draw.circle(DISPLAYSURF, WHITE, (posSnX, posSnY), 2)		
+		pygame.draw.circle(DISPLAYSURF, YELLOW, (posSnX, posSnY), 3)		
 		posSnY -= 1
 		pygame.draw.circle(DISPLAYSURF, WHITE, (posSnX, posSnY), 2)
 		posSnY -= 1
-		pygame.draw.circle(DISPLAYSURF, WHITE, (posSnX, posSnY), 1)
+		pygame.draw.circle(DISPLAYSURF, RED, (posSnX, posSnY), 1)
 		posSnY += 3
 		pygame.draw.circle(DISPLAYSURF, WHITE, (posSnX, posSnY), 2)
 		posSnY += 1
