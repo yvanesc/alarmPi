3495df327b5637e675ee91eaba15a16caff35558
yvanesc <yvan.esc@gmail.com>
2019-01-12 14:09:05 +0100

    up

--- displayPi.py
+++ displayPi.py
@@ -350,12 +350,16 @@ def scrSaveT02(DISPLAYSURF,scrW,scrH):
 	for i in range(len(snow_list)):
  
 		# Draw the snow flake		
-		pygame.draw.circle(DISPLAYSURF, GREY, snow_list[i], 3)				
-		snow_list[i][1] += 2
+						
+		#snow_list[i][1] += 2
 		pygame.draw.circle(DISPLAYSURF, WHITE, snow_list[i], 4)				
+		posSnY = snow_list[i][1]
+		posSnY += 1 
+		posSnX = snow_list[i][0]
+		pygame.draw.circle(DISPLAYSURF, GREY, (posSnX, posSnY), 3)
 
 		# Move the snow flake down one pixel
-		#snow_list[i][1] += 1
+		snow_list[i][1] += 1
 
 		# If the snow flake has moved off the bottom of the screen
 		if snow_list[i][1] > scrH :#or (snow_list[i][1] > 51) and snow_list[i][0] > (scrW - lenDisTime - marge)):        
