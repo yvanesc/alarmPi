36c6cb029e2abd1c77293f7fbed06647f0374fd5
yvanesc <yvan.esc@gmail.com>
2019-01-13 12:38:32 +0100

    up

--- guiMenu03.py
+++ guiMenu03.py
@@ -57,7 +57,7 @@ while True:
 		#display button 1-4 & 5-6
 		
 		#display center
-        displayPi.scrSaveRain(DISPLAYSURF,scrW,scrH)
+        displayPi.scrSaveStars(DISPLAYSURF,scrW,scrH)
 
         pygame.display.update()
         clock.tick(60)  # Limit the frame rate to 60 FPS.
