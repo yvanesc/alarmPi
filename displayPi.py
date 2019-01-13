ebd62bf379d865f6335032ee01cc3ea5e265b2f8
yvanesc <yvan.esc@gmail.com>
2019-01-13 21:56:45 +0100

    up

--- displayPi.py
+++ displayPi.py
@@ -723,7 +723,7 @@ def scrSaveCircle(DISPLAYSURF,scrW,scrH):
 
 		# If the snow flake has moved off the bottom of the screen
 		#lenDisTime = scrW - lenDisTime
-		if snow_list[i][1] > (scrH - 10) or (snow_list[i][1] > 45 and snow_list[i][0] > (scrW - lenDisTime - marge - marge)):        
+		if snow_list[i][1] > (scrH - 10) or (snow_list[i][1] > 30 and snow_list[i][0] > (scrW - lenDisTime - marge*5)):        
 			# Reset it just above the top
 			y = random.randrange(-50, -10)
 			snow_list[i][1] = y
