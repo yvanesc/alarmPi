75afa849ca136e8544a3c9bcd00671fe59756f22
yvanesc <yvan.esc@gmail.com>
2019-01-13 17:02:40 +0100

    up

--- displayPi.py
+++ displayPi.py
@@ -492,7 +492,7 @@ def scrSaveStars(DISPLAYSURF,scrW,scrH):
 		for i in range(40):
 			x = random.randrange(0, scrW)
 			y = random.randrange(0, scrH)
-			sizeStarLst = [random.randrange(2, 25),random.randrange(2, 15)]
+			sizeStarLst = [random.randrange(2, 25),random.randrange(2, 15),random.randrange(2, 10)]
 			sizeStar = random.choice(sizeStarLst)#random.choice(items)		
 			snow_list.append([x, y, sizeStar])			
 		#for i in range(5):			
