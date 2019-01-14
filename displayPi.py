51ccf672e65fa176f42d21ed9c3525a4acf6a660
yvanesc <yvan.esc@gmail.com>
2019-01-15 00:20:19 +0100

    up

--- displayPi.py
+++ displayPi.py
@@ -799,7 +799,7 @@ def scrSaveClock(DISPLAYSURF,scrW,scrH):
 	pts = []
 	for iCirc in range(0,200,1):
 		x = 270 + 60 * math.cos(math.pi / 16 + math.pi * 2 * iCirc / 65)
-		y = 350 + 60 * math.sin(math.pi / 16 + math.pi * 2 * iCirc / 65)
+		y = 380 + 60 * math.sin(math.pi / 16 + math.pi * 2 * iCirc / 65)
 		pts.append([int(x), int(y)])
 	pygame.draw.polygon(DISPLAYSURF, GREY, pts)
  
