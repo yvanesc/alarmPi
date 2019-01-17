15425d3a0d8af3e335b5ee9ae8a2b05656ee9ff5
yvanesc <yvan.esc@gmail.com>
2019-01-17 23:28:54 +0100

    up

--- displayPi.py
+++ displayPi.py
@@ -832,7 +832,7 @@ def txtDisplay(DISPLAYSURF,scrW,scrH):
 def iconeBell(DISPLAYSURF,scrW,scrH):#,status):
 	icBell=pygame.image.load(ic32PathR+ "bell" + ic32PathE)#stateBell +ic32PathE)  		
 	#icBell = pygame.transform.rotate(icBell,90)
-	DISPLAYSURF.blit(icBell, ((scrW/2)+marge, icRectPosY*2))
+	DISPLAYSURF.blit(icBell, (550, 80)
 #angleRot = 0
 def revDisplay():
 	if reverse == 0:
