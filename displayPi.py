38da0ef8019802c7963c8f9461ad904d7b59d3d7
yvanesc <yvan.esc@gmail.com>
2019-01-17 22:46:48 +0100

    fx

--- displayPi.py
+++ displayPi.py
@@ -842,7 +842,7 @@ def scrLeftButt(DISPLAYSURF,scrW,scrH):
 def powerCirc(DISPLAYSURF,scrW,scrH):
 	pts = []
 	for iCirc in range(1,100,1):
-		x = 800 + 20 * math.cos(math.pi / 16 + math.pi * 2 * iCirc / 65)
+		x = 600 + 20 * math.cos(math.pi / 16 + math.pi * 2 * iCirc / 65)
 		y = 80 + 20 * math.sin(math.pi / 16 + math.pi * 2 * iCirc / 65)
 		pts.append([int(x), int(y)])
 	pygame.draw.polygon(DISPLAYSURF, WHITE, pts)
