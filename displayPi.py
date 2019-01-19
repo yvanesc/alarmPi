568248c901c518acda57eb717796ac94c5ca31a4
yvanesc <yvan.esc@gmail.com>
2019-01-19 01:46:33 +0100

    up

--- displayPi.py
+++ displayPi.py
@@ -840,9 +840,9 @@ def txtDisplayRev(DISPLAYSURF,scrW,scrH):
 	displayDate = fontSel.render(date2Display, True, ORANGE)
 	displayDay = fontSel.render(disDay, True, ORANGE)
 	lenDisTime = displayTime.get_width()	
-	DISPLAYSURF.blit(revDisplay(displayTime), (20, scrH - lenDisTime)) 
-	DISPLAYSURF.blit(revDisplay(displayDate), (150, scrH - lenDisTime)) 
-	DISPLAYSURF.blit(revDisplay(displayDay), (280, scrH - lenDisTime)) 	
+	DISPLAYSURF.blit(revDisplay(displayTime), (20, 0))#scrH - lenDisTime)) 
+	DISPLAYSURF.blit(revDisplay(displayDate), (150, 0))
+	DISPLAYSURF.blit(revDisplay(displayDay), (280, 0))
 
 def iconeBell(DISPLAYSURF,scrW,scrH):#,status):
 	icBell=pygame.image.load(ic32PathR+ "bell" + ic32PathE)#stateBell +ic32PathE)  		
@@ -889,7 +889,7 @@ def scrMxScrSavLfBtVert(DISPLAYSURF,scrW,scrH):
 	DISPLAYSURF.fill(BLACK)
 	scrSaveClock(DISPLAYSURF,scrW,scrH)
 	scrLeftButt(DISPLAYSURF,scrW,scrH)
-	powerCirc(DISPLAYSURF,scrW,scrH)
+	#powerCirc(DISPLAYSURF,scrW,scrH)
 	statuSys(DISPLAYSURF,scrW,scrH)
 	txtDisplayRev(DISPLAYSURF,scrW,scrH)
 	#rightIco(DISPLAYSURF,scrW,scrH)
