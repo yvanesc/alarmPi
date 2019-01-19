05e78153faaee215eaf813f88829cc6cc005d8ab
yvanesc <yvan.esc@gmail.com>
2019-01-19 01:59:19 +0100

    up

--- displayPi.py
+++ displayPi.py
@@ -839,10 +839,14 @@ def txtDisplayRev(DISPLAYSURF,scrW,scrH):
 	displayTime = fontSelL.render(time2Display, True, ORANGE)
 	displayDate = fontSel.render(date2Display, True, ORANGE)
 	displayDay = fontSel.render(disDay, True, ORANGE)
+	displayNxtAl = fontSel.render("Next Alarm : 08:00", True, ORANGE)
 	lenDisTime = displayTime.get_width()	
 	DISPLAYSURF.blit(revDisplay(displayTime), (60, 60))#scrH - lenDisTime)) 
 	DISPLAYSURF.blit(revDisplay(displayDate), (200, 60))
 	DISPLAYSURF.blit(revDisplay(displayDay), (280, 60))
+	displayNxtAl = pygame.transform.rotate(displayNxtAl,90)
+	DISPLAYSURF.blit(displayNxtAl, (480, 200))
+
 
 def iconeBell(DISPLAYSURF,scrW,scrH):#,status):
 	icBell=pygame.image.load(ic32PathR+ "bell" + ic32PathE)#stateBell +ic32PathE)  		
@@ -854,6 +858,7 @@ def iconeBellRev(DISPLAYSURF,scrW,scrH):#,status):
 	#ic2Dis=revDisplay(icBell)
 
 	DISPLAYSURF.blit(revDisplay(icBell),(485, 65))	
+
 	#angleRot = 0
 def revDisplay(ic2Use):
 	if iniPi.angleRot < 90:
